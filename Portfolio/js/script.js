7eb0c4906310bdc0a575045d039e6e15de7ee24b
PrathameshAdinawar <64590280+PrathameshAdinawar@users.noreply.github.com>
2024-06-27 12:40:45 +0530

    tp

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,17 +1,17 @@
 (function() {
-    $( "#button1" ).click(function() {
-      $( "#button1" ).addClass( "onclic", 250, validate);
+    $( "#button" ).click(function() {
+      $( "#button" ).addClass( "onclic", 250, validate);
     });
   
     function validate() {
       setTimeout(function() {
-        $( "#button1" ).removeClass( ".onclic" );
-        $( "#button1" ).addClass( ".validate", 450, callback );
+        $( "#button" ).removeClass( "onclic" );
+        $( "#button" ).addClass( "validate", 450, callback );
       }, 2250 );
     }
       function callback() {
         setTimeout(function() {
-          $( "#button1" ).removeClass( ".validate" );
+          $( "#button" ).removeClass( "validate" );
         }, 1250 );
       }
     });
